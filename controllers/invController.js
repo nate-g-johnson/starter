ec3db78c12614b12b2012612b6470cd71aa8e04e
nate-g-johnson <n_george_j@outlook.com>
2025-09-20 10:51:42 -0400

    footer based malware and css update

--- controllers/invController.js
+++ controllers/invController.js
@@ -7,16 +7,30 @@ const invCont = {}
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
+  try {
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    let nav = await utilities.getNav()
+
+    if (!data || data.length === 0) {
+      return res.status(404).render("errors/error", {
+        title: "Not Found",
+        nav,
+        message: "Sorry, that classification does not exist."
+      })
+    }
+
+    const grid = await utilities.buildClassificationGrid(data)
+    const className = data[0].classification_name
+
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    })
+  } catch (err) {
+    next(err)
+  }
 }
 
 /* ***************************
--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = new express.Router();
+const utilities = require("../utilities/");
 
 router.get("/trigger-error", (req, res, next) => {
   next(new Error("Intentional 500 error"));
+});
+
+//error link in nav
+router.get("/error", async (req, res, next) => {
+  try {
+    let nav = await utilities.getNav()
+    res.render("errors/error", {
+      title: "Error Page",
+      nav,
+      message: "Sorry, an error occurred."
+    })
+  } catch (err) {
+    next(err)
+  }
 });
 
 module.exports = router;
