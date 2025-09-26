08aa66df0dd1e9723446f49d4070588a40ef050b
nate-g-johnson <n_george_j@outlook.com>
2025-09-26 12:09:48 -0400

    adding the require account validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,6 +4,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 //Routes
 
@@ -16,7 +17,7 @@ router.get("/register", utilities.handleErrors(accountController.buildRegister))
 // Process the registration data
 router.post(
   "/register",
-  regValidate.registationRules(),
+  regValidate.registrationRules(),
   regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount)
 )
